6b14915261106d59c8c408ace883d98f67b29bba
SoloVova <vsolo0@gmail.com>
2022-01-03 12:34:27 +0200

    0.28 ErrorHandler

--- SoloVova.Delivery.Backend.WebApi/Startup.cs
+++ SoloVova.Delivery.Backend.WebApi/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +8,7 @@ using SoloVova.Delivery.Backend.Application;
 using SoloVova.Delivery.Backend.Application.Interfaces;
 using SoloVova.Delivery.Backend.Application.Mapping;
 using SoloVova.Delivery.Backend.Persistence;
+using SoloVova.Delivery.Backend.WebApi.Middleware;
 
 namespace SoloVova.Delivery.Backend.WebApi{
     public class Startup{
@@ -33,6 +29,10 @@ namespace SoloVova.Delivery.Backend.WebApi{
                     policy.AllowAnyOrigin();
                 });
             });
+            
+            // services.AddTransient<IConfigureOptions<SwaggerGenOptions>,
+            //     ConfigureSwaggerOptions>();
+            // services.AddSwaggerGen();
         }
 
         public IConfiguration Configuration{ get; }
@@ -46,13 +46,20 @@ namespace SoloVova.Delivery.Backend.WebApi{
             if (env.IsDevelopment()){
                 app.UseDeveloperExceptionPage();
             }
-
+            
+            // app.UseSwagger();
+            //
+            // app.UseSwaggerUI(options =>
+            // {
+            //     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            //     options.RoutePrefix = string.Empty;
+            // });
+            app.UseCustomExceptionHandler();
             app.UseRouting();
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
 
             app.UseEndpoints(endpoints => {
-                //endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
                 endpoints.MapControllers();
             });
         }
